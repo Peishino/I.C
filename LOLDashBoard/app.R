06803a291b457d3b3d6b6114a88d30dcd04cc00f
Peishino <gabriel.moreira419@gmail.com>
2023-12-26 09:40:10 -0300

    treating NA and update app

--- LOLDashBoard/app.R
+++ LOLDashBoard/app.R
@@ -1,40 +1,79 @@
 library(shiny)
 library(shinydashboard)
+library(plotly)
 library(ggplot2)
 
-ui <- dashboardPage(skin = "black",
-                    dashboardHeader(title = "League of legends worlds"),
-                    dashboardSidebar(
-                      selectInput("Player", "Selecione o jogador", choices =  NULL),
-                      selectInput("Season", "Filtrar por temporada", choices = NULL),
-                      plotOutput("WinRate")
-                    ),
-                    dashboardBody(
-                      fluidRow(
-                        box(width = 12, status = "warning", solidHeader = TRUE,
-                            title = "Desempenho do jogador por partida",
-                            infoBoxOutput("Team", width = 3),
-                            infoBoxOutput("KDA", width = 3),
-                            infoBoxOutput("CreepScore", width = 3),
-                            infoBoxOutput("Damage", width = 3)
-                        ),
-                        column(9,
-                               box(width = 12, height = 211, status = "primary", solidHeader = T,
-                                   plotOutput("KDA_Season", height = 190)
-                               ),
-                               box(width = 12, height = 211, status = "success", solidHeader = T,
-                                   plotOutput("Wr_Season", height = 190)
-                               )
-                        ),
-                        column(3,
-                               box(width = 12,height = 444,
-                                   plotOutput("cs_min"),
-                                   plotOutput("ks")
-                                   ),
-                              
-                        )
-                      )
-                    )
+ui <- dashboardPage(
+  skin = "black",
+  dashboardHeader(title = "League of legends worlds"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Jogadores", tabName = "dashboard1", icon = icon("chart-line")),
+      menuItem("Personagens", tabName = "dashboard2", icon = icon("chart-bar")),
+      menuItem("Partidas", tabName = "dashboard3", icon = icon("chart-area"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # Conteúdo para a primeira aba 
+      tabItem(
+        tabName = "dashboard1",
+        fluidRow(
+          box(width = 12, status = "warning", solidHeader = TRUE,
+              title = "Desempenho do jogador por partida",
+              infoBoxOutput("Team", width = 3),
+              infoBoxOutput("KDA", width = 3),
+              infoBoxOutput("CreepScore", width = 3),
+              infoBoxOutput("Damage", width = 3)
+          ),
+          column(9,
+                 box(width = 12, height = 211, status = "primary", solidHeader = T,
+                     plotOutput("KDA_Season", height = 190)
+                 ),
+                 box(width = 12, height = 211, status = "success", solidHeader = T,
+                     plotOutput("Wr_Season", height = 190)
+                 )
+          ),
+          column(3,
+                 box(width = 12,height = 444,
+                     selectInput("Player", "Selecione o jogador", choices =  NULL),
+                     selectInput("Season", "Filtrar por temporada", choices = NULL)
+                 )
+          )
+        )
+      ),
+      # Conteúdo para a segunda aba 
+      tabItem(
+        tabName = "dashboard2",
+        fluidRow(
+          box(width = 12, height = 2,
+              selectInput("Season", "Filtrar por temporada", choices = "Todas"),
+              selectInput("Lane", "Filtrar por rota", choices = "Todas")
+          ),
+          column(6,
+                 box(width = 12, status = "primary", solidHeader = T,
+                     plotOutput("GamesContests")
+                     ),
+                 box(width = 12, status = "primary", solidHeader = T,
+                     plotOutput("WinLose")
+                 )
+          column(6,
+                 box(),
+                 box())
+          
+        )
+      ),
+      # Conteúdo para a terceira aba 
+      tabItem(
+        tabName = "dashboard3",
+        fluidRow(
+          box(width = 12, status = "warning", solidHeader = TRUE,
+              title = "Conteúdo do Dashboard 3"
+          )
+        )
+      )
+    )
+  )
 )
 
 server <- function(input, output, session) {
@@ -91,14 +130,14 @@ server <- function(input, output, session) {
   output$CreepScore <- renderInfoBox({
     infoBox(
       fill = TRUE,
-      title = "Média de Minions",
-      value = mean(Players_Inputs()$creep_score),
+      title = "Média de Minions por minuto",
+      value = mean(Players_Inputs()$cs.min),
       icon = icon("chess-pawn"),
       color = "yellow",
       
     )
   })
-  
+  60*21.91
   output$Damage <- renderInfoBox({
     infoBox(
       fill = TRUE,
@@ -126,15 +165,15 @@ server <- function(input, output, session) {
     df <- PlayersData()
     df_filtered <- df %>% filter(player == input$Player)
     
-    ggplot(df_filtered, aes(x = season)) +
-      geom_area(aes(y = wins, fill = "Win"), position = "identity", alpha = 0.5) +
-      geom_area(aes(y = loses, fill = "Lose"), position = "identity", alpha = 0.5) +
+    ggplot(df_filtered, aes(x = factor(season), group = 1)) +
+      geom_point(aes(y = wins, color = "Win"), size = 3) +
+      geom_point(aes(y = loses, color = "Lose"), size = 3) +
       geom_line(aes(y = wins, color = "Win"), size = 1.5) +
       geom_line(aes(y = loses, color = "Lose"), size = 1.5) +
-      scale_fill_manual(values = c("Win" = "green", "Lose" = "red")) +
       scale_color_manual(values = c("Win" = "green", "Lose" = "red")) +
       labs(title = "Vitórias e Derrotas por Temporada", x = "Temporada", y = "Quantidade") +
-      theme_minimal()
+      theme_minimal() +
+      scale_x_discrete(breaks = unique(df_filtered$season))
   })
   
   
