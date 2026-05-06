f92e3de688532e68748e95bab38657edc9b397e7
Gabriel Moreira <gabriel.moreira419@gmail.com>
2026-05-06 19:15:50 -0300

    alterações na validação da série temporal
    adicionando validação por sliding window do modelo de séries temporais

--- RedesNeurais/Redes Neurais/Modelo_series_temporais_Petr4.R
+++ RedesNeurais/Redes Neurais/Modelo_series_temporais_Petr4.R
@@ -14,19 +14,33 @@ library(lmtest)
 
 ativo <- "PETR4.SA" 
 inicio <- "2007-01-01"
-fim <- Sys.Date() # Data de hoje
+fim <- "2026-05-06" 
 
 # dados do Yahoo Finance
 getSymbols(ativo, src = "yahoo", from = inicio, to = fim, auto.assign = TRUE)
 dados <- as.numeric(Ad(get(ativo)))
 dados_petr <- as.numeric(Ad(get(ativo))) # Pegamos apenas a coluna "Adjusted" (Preço Fechamento Ajustado por dividendos/desdobramentos)
-dados_petr <- dados_petr[1:(length(dados_petr)-5)]
+dados_petr <- dados_petr[1:(length(dados_petr)-10)]
 petr_ts <- ts(dados_petr, start = 1, frequency = 1) 
-ts.plot(petr_ts, main = "Cotação PETR4", ylab = "Preço")
+ts.plot(petr_ts, main = "Fechamento Ajustado PETR4", ylab = "Preço", line.width = 2)
 
-acf(petr_ts, lag.max = 40, main="ACF Preço") 
-pacf(petr_ts, lag.max = 40, main="PACF Preço")
-adf.test(petr_ts) #  p valor > 0.05 não estacionário
+library(ggplot2)
+
+ggplot(data = data.frame(Tempo = 1:length(petr_ts), Preco = as.numeric(petr_ts)), aes(x = Tempo, y = Preco)) +
+  geom_line(color = "#0f0f0f", size = 1) +
+  labs(title = "", x = "", y = "") +
+  theme_minimal()+
+  theme(
+    axis.text.x = element_blank(),
+    panel.grid.major.x  = element_blank(),
+    panel.grid.minor.x  = element_blank(),
+    axis.text = element_text(size = 30)
+  )
+
+
+acf(petr_ts, lag.max = 40, main="ACF Preço") # q = 0
+pacf(petr_ts, lag.max = 40, main="PACF Preço") # p = 1
+adf.test(petr_ts) #  p valor > 0.05 não estacionário, provavelmente d = 1
 
 media <- vector()
 desvio <- vector()
@@ -60,7 +74,7 @@ cox.stuart.test(diff_petr) # p valor > 0.05, sem tendência
 fisher.g.test(diff_petr)# p valor > 0.05, não tem sazonalidade periódica forte
 
 # Modelo ARIMA
-modelo <- auto.arima(petr_ts,trace=TRUE) # c(1,1,1)
+modelo <- auto.arima(petr_ts,trace=TRUE)
 modelo
 ?auto.arima
 
@@ -80,7 +94,7 @@ Box.test(m1$residuals, type = c("Box-Pierce")) # p-valor > 0.05, resíduos são 
 cpgram(m1$residuals, main="Cpgram M1") # dentro da faixa azul, está ok
 
 # segundo modelo
-m2 <- Arima(petr_ts, order = c(0,1,1))
+m2 <- Arima(petr_ts, order = c(1,1,0))
 coeftest(m2) # nenhum dos coeficientes são significativos (p-valor > 0.05)
 
 # Diagnóstico M2
@@ -93,39 +107,130 @@ BIC(m1)
 BIC(m2)
 AIC(m1)
 AIC(m2)
-# m1 é melhor que m2
+# m2 foi melhor que m1
 
-melhor_modelo <- m1 
+melhor_modelo <- m2
 
 accuracy(melhor_modelo)
 'accuracy(melhor_modelo)
 ME      RMSE       MAE         MPE     MAPE     MASE        ACF1
 Training set 0.005044264 0.2450356 0.1514653 -0.00126123 1.907175 1.001329 -0.00532868'
 
-previsao <- forecast(melhor_modelo, h = 5, level=c(95))
+previsao <- forecast(melhor_modelo, h = 10, level=c(95))
 
 plot(previsao, lwd = 2, col="black", xlab= "Tempo", main="Previsão PETR4 (Log-Price)")
 
 #zoom
-plot(previsao, lwd = 2, col="black", xlab= "Tempo", main="Previsão PETR4", xlim=c(4700,4755),ylim=c(28,42))
+plot(previsao, lwd = 2, col="black", xlab= "Tempo", main="Previsão PETR4", xlim=c(4780,4795),ylim=c(34,55))
 lines(dados, col="black", lwd=2)#legenda
 #colocar legenda
 legend("topleft", legend=c("Dados Reais", "Previsão"),
        col=c("black", "blue"), lty=1, cex=0.8)
 
+
+dados_ggplot <- dados %>%
+  tail(30) %>%
+  data.frame(preco = .)
+
+ggplot(dados_ggplot, aes(x = 1:nrow(dados_ggplot), y = preco)) +
+  geom_line(aes(color = "Real"),size = 1) +
+  geom_line(data = data.frame(x = (20+1):30, y = previsao$mean), aes(x = x, y = y,color = "Previsão"), size = 1, linetype = "dashed")+
+  labs(title = "", x = "", y = "") +
+  theme_minimal() +
+  scale_color_manual(values = c("Real" = "black", "Previsão" = "blue")) +
+  theme(
+    axis.text.x = element_blank(),
+    panel.grid.major.x  = element_blank(),
+    panel.grid.minor.x  = element_blank(),
+    axis.text = element_text(size = 30),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 30)
+  )
 # comparar valores reais com a média do modelo
 previsao$mean
-dados[(length(dados)-4):length(dados)]
+dados[(length(dados)-9):length(dados)]
 
 diferencas <- dados[(length(dados)-4):length(dados)] - previsao$mean
 diferencas
 resultado <- data.frame(
-  Dia = 1:5,
+  Dia = 1:10,
   Previsao = round(previsao$mean, 2),
   Real = round(dados[(length(dados)-4):length(dados)], 2),
   Diferenca = round(diferencas, 2)
 )
 resultado
-# o modelo está até bom para prever valores de fechamento, mas ainda não explica completamente
-# e eu fiz só pra 5 dias, se eu colocar mais o modelo perde completamente a acurácia
+# não é um bom resultado, o modelo não lida bem com a não linearidade (esperado)
+# eu não consigo avaliar corretamente o modelo de forma consistente devido a previsão de muitos dias a frente
 
+rsme <- sqrt(mean((resultado$Real - resultado$Previsao)^2))
+rsme
+mape <- mean(abs(resultado$Real - resultado$Previsao) / resultado$Real) * 100
+mape
+
+
+# uma outra forma de validar seria utilizar o rolling forecast ou sliding window
+# onde consigo prever somente um dia a frente
+# mas consigo atualizar o modelo com o último dia e prever o próximo dia, e assim por diante, até chegar no final da série
+
+# Quantidade de dias que queremos prever no total
+n_janela <- 10 
+n_total <- length(dados)
+
+# Vetores vazios para guardar os resultados do loop
+previsoes_1_passo <- numeric(n_janela)
+valores_reais <- numeric(n_janela)
+limite_inf <- numeric(n_janela)
+limite_sup <- numeric(n_janela)
+
+# Loop da janela deslizante
+for (i in 1:n_janela) {
+
+  fim_treino <- n_total - n_janela + (i - 1)
+  
+
+  dados_treino <- ts(dados[1:fim_treino], start = 1, frequency = 1)
+  modelo_atual <- Arima(dados_treino, order = c(1,1,0))
+  prev <- forecast(modelo_atual, h = 1)
+  previsoes_1_passo[i] <- as.numeric(prev$mean)
+  limite_inf[i] <- as.numeric(prev$lower)
+  limite_sup[i] <- as.numeric(prev$upper)
+  valores_reais[i] <- dados[fim_treino + 1]
+}
+
+# Criando o DataFrame com todos os dados
+resultado_sliding <- data.frame(
+  Dia = 1:n_janela,
+  Real = valores_reais,
+  Previsao = previsoes_1_passo,
+  Limite_Inf = limite_inf,
+  Limite_Sup = limite_sup
+)
+
+print(round(resultado_sliding, 2))
+
+ggplot(resultado_sliding, aes(x = Dia)) +
+  geom_ribbon(aes(ymin = Limite_Inf, ymax = Limite_Sup, fill = "IC 95%"), alpha = 0.2) +
+  geom_line(aes(y = Previsao, color = "Previsão"), size = 1.2, linetype = "dashed") +
+  geom_point(aes(y = Previsao, color = "Previsão"), size = 3) +
+  geom_line(aes(y = Real, color = "Real"), size = 1.2) +
+  geom_point(aes(y = Real, color = "Real"), size = 3) +
+  scale_color_manual(values = c("Real" = "#0f0f0f", "Previsão" = "blue")) +
+  scale_fill_manual(values = c("IC 95%" = "blue")) +
+  labs(title = "Validação Sliding Window: Real vs Previsão (h=1)",
+       x = "Dias de Teste (Últimos 10 dias)", 
+       y = "Preço (R$)",
+       color = "Série:",
+       fill = "Intervalo:") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 16, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    legend.position = "bottom",
+    legend.text = element_text(size = 12)
+  )
+
+
+# já está melhor, mas ainda é péssimo. O modelo não entende a alta volatilidade do mercado.
+# ele simplesmente faz uma "linha reta", ou seja, prevê que o valor de amanhã seja praticamente o mesmo de hoje
+# o que não é uma boa previsão para o mercado financeiro.
--- RedesNeurais/Redes Neurais/Rede_neural_MLP_petr4.R
+++ RedesNeurais/Redes Neurais/Rede_neural_MLP_petr4.R
@@ -46,7 +46,7 @@ teste  <- base_ml[(tam_treino+1):nrow(base_ml), ]
 # para criação do modelo de rede neural, precisamos criar uma fórmula
 # no formato Alvo ~ Lag_1 + Lag_2 + ... + Lag_n
 formula_nn <- as.formula(paste("Alvo ~", paste(names(base_ml)[-1], collapse = " + ")))
-set.seed(123)
+set.seed(419)
 
 # No código MVP_MLP_petr4.R há uma função para testar várias arquiteturas
 
@@ -58,26 +58,88 @@ modelo_nn <- neuralnet(formula_nn,
                        linear.output = TRUE,
                        threshold = 0.1,
                        lifesign = "full",
-                       lifesign.step = 1000)
-?neuralnet
+                       rep = 3,
+                       lifesign.step = 10000)
+#?neuralnet
 # hidden = c(4,2) significa que a rede tem 2 camadas ocultas, a primeira com 4 neurônios e a segunda com 2 neurônios
 #plot(modelo_nn)
 previsoes_nn <- neuralnet::compute(modelo_nn, teste[, -1]) # tirar a coluna alvo
 
 #plotar previsões vs real
+rmse <- sqrt(mean((teste$Alvo - previsoes_nn$net.result)^2))
 ggplot() +
   geom_line(data = teste, aes(x = 1:nrow(teste), y = Alvo), color = "blue", size = 1) +
   geom_line(data = as.data.frame(previsoes_nn$net.result), aes(x = 1:nrow(teste), y = V1), color = "red", size = 1) +
   labs(title = "Previsões vs Real", x = "Tempo", y = "Valor Normalizado") +
   theme_minimal() +
   scale_color_manual(values = c("Real" = "blue", "Previsto" = "red")) +
-  theme(legend.title = element_blank())
+  theme(legend.title = element_blank())+
+  geom_text(aes(x = 5, y = 3), label = paste("RMSE:", round(rmse, 4)), color = "black", size = 5)
+
+#despadronização
+previsoes_nn_valores <- previsoes_nn$net.result * desvios + medias
+real_valores <- teste$Alvo * desvios + medias
 
 
-previsoes_nn_valores <- previsoes_nn$net.result * (maxs - mins) + mins
-real_valores <- teste$Alvo * (maxs - mins) + mins
 resultados <- data.frame(
-  Real = as.numeric(real_valores),
-  Previsto = as.numeric(previsoes_nn_valores)
+  Retorno_Real = as.numeric(real_valores),
+  Retorno_Previsto = as.numeric(previsoes_nn_valores)
 )
-print(resultados)
+
+
+ggplot(resultados, aes(x = Retorno_Real, y = Retorno_Previsto)) +
+  geom_point(color = "blue", size = 2) +
+  geom_abline(slope = 1, intercept = 0, color = "red", linetype = "dashed") +
+  labs(title = "Previsão vs Real (Despadronizado)", x = "Retorno Real", y = "Retorno Previsto") +
+  theme_minimal()
+
+precos_originais <- as.numeric(Ad(PETR4.SA))
+n_teste <- nrow(resultados)
+
+resultados$Preco_Real <- tail(precos_originais, n_teste)
+
+indice_inicio_teste <- length(precos_originais) - n_teste
+precos_base_ontem <- precos_originais[indice_inicio_teste:(length(precos_originais) - 1)]
+
+resultados$Preco_Previsto <- precos_base_ontem * exp(resultados$Retorno_Previsto)
+
+#plot da ts real vs previsto
+ggplot(resultados, aes(x = 1:n_teste)) +
+  geom_line(aes(y = Preco_Real, color = "Real"), size = 1) +
+  geom_line(aes(y = Preco_Previsto, color = "Previsto"), size = 1, linetype = "dashed") +
+  labs(title = "", x = "Tempo", y = "Preço (R$)") +
+  theme_minimal() +
+  scale_color_manual(values = c("Real" = "black", "Previsto" = "blue")) +
+  theme(
+    legend.title = element_blank(),
+    legend.text = element_text(size = 10),
+    panel.grid.minor.y = element_blank(),
+    panel.border = element_blank()
+    
+  )
+
+ggplot(resultados, aes(x = 1:n_teste)) +
+  geom_line(aes(y = Preco_Real, color = "Real"), size = 1) +
+  # Filtra apenas os últimos 10 registros para a linha de previsão
+  geom_line(data = tail(resultados, 10), 
+            aes(x = (n_teste - 9):n_teste, y = Preco_Previsto, color = "Previsto"), 
+            size = 1, linetype = "dashed") +
+  labs(title = "", x = "", y = "") +
+  theme_minimal() +
+  scale_color_manual(values = c("Real" = "black", "Previsto" = "blue")) +
+  theme(
+    axis.text.x = element_blank(),
+    panel.grid.major.x  = element_blank(),
+    panel.grid.minor.x  = element_blank(),
+    axis.text = element_text(size = 30),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 30)
+  )
+
+?theme
+
+rmse_real <- sqrt(mean((resultados$Preco_Real - resultados$Preco_Previsto)^2))
+cat("RMSE em R$:", round(rmse_real, 2), "\n")
+
+mape_real <- mean(abs(resultados$Preco_Real - resultados$Preco_Previsto) / resultados$Preco_Real) * 100
+cat("MAPE em %:", round(mape_real, 2), "%\n")
